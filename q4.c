ff8e1375ae00b92a0815596a82c63fac26805811
Sooraj2002 <jhasooraj56@gmail.com>
2021-01-07 21:26:26 +0530

    some changes in dev

---
+++ q4.c
@@ -0,0 +1,30 @@
+// AUTHOUR SOORAJ  JHA 
+// BCA 1 YEAR
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int code;
+clrscr();
+printf("\n PROGRAM TO CHECK SWITCH STATEMENT ");
+printf("\n ENTER THE COLOUR CODE : ");
+scanf("%d",&code);
+if(code==1)
+{
+printf("\n COLOUR IS RED ");
+}
+else if (code==1)
+{
+printf("\n COLOUR IS GREEN");
+}
+else if (code==2)
+{
+printf("\n COLUR IS WHITE ");
+}
+else if(code==3)
+{
+printf("\n COLUR IS WHITE ");
+}
+printf("\n END OF PROGRAM ");
+getch();
+}
